1ace685915693552c4c2b4b8b8569d6543cb887d
hawky358 <hawkway@gmail.com>
2023-05-22 16:04:34 +0200

    Reflecting new name

--- pyhyypapi/__main__.py
+++ pyhyypapi/__main__.py
@@ -1,5 +1,5 @@
 """Google Firebase push receiver example."""
-from pyhyypapi.push_receiver import run_example
+from pyhyypapihawkmod.push_receiver import run_example
 
 if __name__ == "__main__":
     run_example()
