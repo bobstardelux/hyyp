0d25bc38122124f9afdeabc3e6e5a61cd4b965fc
hawky358 <hawkway@gmail.com>
2023-05-28 17:17:53 +0200

    Bumped to 1.0.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='pyhyypapihawkmod',
-    version="0.0.0.6",
+    version="1.0.0",
     license='Apache Software License 2.0',
     author='hawky358 (Original code by Renier Moorcroft)',
     author_email='hawky358@users.github.com',
